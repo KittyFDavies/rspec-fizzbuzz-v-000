eb09230e98445bde641b97027767922f6358eeeb
Kitty Davies <kittyfdavies@gmail.com>
2018-02-27 20:29:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-      if int % 5 == 0
-        "FizzBuzz"
+      if int % 5 == 0 # if the number int is also divisible by 5
+        "FizzBuzz" 
       else
         "Fizz"
       end
   elsif int % 5 == 0 # if the number int is divisible by 5
-    if int % 3 == 0
+    if int % 3 == 0 # if the number int is also divisible by 3
       "FizzBuzz"
     else
       "Buzz"
     end
   else
-    nil
+    nil # if the number int is divisible by none, return nil
   end
 end
